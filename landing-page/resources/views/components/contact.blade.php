5532122a9edbb8ef25847fe4c0f29a4586f5bb4c
SergiRoigSanchez <sergiroig@iesebre.com>
2024-02-05 16:00:18 +0100

    Sync

--- landing-page/resources/views/components/contact.blade.php
+++ landing-page/resources/views/components/contact.blade.php
@@ -1,40 +1,40 @@
-<div id="contacte" class="relative isolate overflow-hidden bg-cyan-900 py-16 sm:py-24 lg:py-32">
-        <div class="bg-[#2362e8] relative z-[1] container m-auto px-6 md:px-12 rounded-xl h-[1000px] w-[1000px] text-center">
+<div id="contacte" class="relative isolate overflow-hidden bg-[#2362e8] py-16 sm:py-24 lg:py-32">
+        <div class="relative z-[1] container mx-auto px-6 md:px-12 rounded-xl h-[1000px] w-[1000px] text-center">
             {{--        <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">--}}
             {{--            <div class="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>--}}
             {{--        </div>--}}
             <div class="mx-auto max-w-2xl text-center">
-                <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Contacte</h2>
+                <h2 class="text-3xl font-bold tracking-tight text-white">Contacte</h2>
                 <p class="mt-2 text-lg leading-8 text-white">T'interessa el nostre producte? Contacta'ns!</p>
             </div>
             <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
                 <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                     <div>
-                        <label for="first-name" class="block text-sm font-semibold leading-6 text-white">Nom *</label>
+                        <label for="first-name" class="text-left block text-sm font-semibold leading-6 text-white">Nom *</label>
                         <div class="mt-2.5">
                             <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
                     <div>
-                        <label for="last-name" class="block text-sm font-semibold leading-6 text-white">Cognoms *</label>
+                        <label for="last-name" class="text-left block text-sm font-semibold leading-6 text-white">Cognoms *</label>
                         <div class="mt-2.5">
                             <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
                     <div class="sm:col-span-2">
-                        <label for="company" class="block text-sm font-semibold leading-6 text-white">Empresa</label>
+                        <label for="company" class="text-left block text-sm font-semibold leading-6 text-white">Empresa</label>
                         <div class="mt-2.5">
                             <input type="text" name="company" id="company" autocomplete="organization" class="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
                     <div class="sm:col-span-2">
-                        <label for="email" class="block text-sm font-semibold leading-6 text-white">Correu electrònic *</label>
+                        <label for="email" class="text-left block text-sm font-semibold leading-6 text-white">Correu electrònic *</label>
                         <div class="mt-2.5">
                             <input type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
                     <div class="sm:col-span-2">
-                        <label for="phone-number" class="block text-sm font-semibold leading-6 text-white">Número de telèfon</label>
+                        <label for="phone-number" class="text-left block text-sm font-semibold leading-6 text-white">Número de telèfon</label>
                         <div class="relative mt-2.5">
                             <div class="absolute inset-y-0 left-0 flex items-center">
                                 <label for="country" class="sr-only">Country</label>
@@ -51,7 +51,7 @@
                         </div>
                     </div>
                     <div class="sm:col-span-2">
-                        <label for="message" class="block text-sm font-semibold leading-6 text-white">Missatge</label>
+                        <label for="message" class="text-left block text-sm font-semibold leading-6 text-white">Missatge</label>
                         <div class="mt-2.5">
                             <textarea name="message" id="message" rows="4" class="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                         </div>
